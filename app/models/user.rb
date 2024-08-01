a4d7461e4144934f679de3e0757c137a06eb22bb
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-01 21:02:16 +0900

    ユーザーログイン機能修正１
    テストコードの実装
    テストコードの画像あり

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-           # バリデーション
-           validates :nickname,presence: true
-            
-           validates :last_name, presence: true
-           validates :first_name, presence: true
-           validates :last_name_kana, presence: true
-           validates :first_name_kana, presence: true
-           validates :birthday, presence: true
+  # 文字の指定 
+  ALPHANUMERIC_REGEX = /\A[a-zA-Z0-9]+\z/
+  FULL_WIDTH_REGEX = /\A[^\x00-\x7F]+\z/
+  KATAKANA_REGEX = /\A[\p{Katakana}]+\z/
 
-
+  # バリデーション
+  validates :nickname,presence: true
+  validates :password,presence: true, format:{with: ALPHANUMERIC_REGEX}, length:{minimum: 6}
+  validates :last_name, presence: true, format:{with: FULL_WIDTH_REGEX}
+  validates :first_name, presence: true, format:{with: FULL_WIDTH_REGEX}
+  validates :last_name_kana, presence: true, format:{with: KATAKANA_REGEX}
+  validates :first_name_kana, presence: true, format:{with: KATAKANA_REGEX}
+  validates :birthday, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,7 +70,6 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
       end
 
-
       it '名前（性）が空では登録できない' do
         @user.last_name = ''
         @user.valid?
@@ -99,6 +98,48 @@ RSpec.describe User, type: :model do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = '例えば'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+  
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'ttest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+        
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.last_name_kana = 'tuseday'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.first_name_kana = 'sanday'
+        @user.valid?
+            expect(@user.errors.full_messages).to include("First name kana is invalid")
       end
     end
   end
