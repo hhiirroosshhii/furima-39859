2107649f8815fe27b6403030865fff45713a7765
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-02 19:15:28 +0900

    アクティブハッシュのアソシエーション
    アソシエーションをまとめていたらばエラーになったので解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,11 @@ class Item < ApplicationRecord
 
   # アクティブハッシュ
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :cost, :area, :needday
-
+  belongs_to_active_hash :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :needday
   # バリデーション
   validates :product, :explanation, :price, :user, presence: true
   validates :category_id, :status_id, :cost_id, :area_id, :needday_id, presence: true, numericality:{other_than: 1, message: "can't be blank"}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#edit"
+  root to: "items#index"
   resources :items
 
 
