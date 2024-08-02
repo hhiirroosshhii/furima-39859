77ebb2f39faf735668e17c9d2b725757b393a597
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-02 14:25:29 +0900

    ユーザーログイン機能修正３
     STRONG_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]+\z/
    validatesの字体の制限を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   # 文字の指定 
-  ALPHANUMERIC_REGEX = /\A[a-zA-Z0-9]+\z/
+  STRONG_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]+\z/
   FULL_WIDTH_REGEX = /\A[^\x00-\x7F]+\z/
   KATAKANA_REGEX = /\A[\p{Katakana}]+\z/
 
   # バリデーション
   validates :nickname,presence: true
-  validates :password,presence: true, format:{with: ALPHANUMERIC_REGEX}, length:{minimum: 6}
+  validates :password,presence: true, format:{with: STRONG_PASSWORD_REGEX}, length:{minimum: 6}
   validates :last_name, presence: true, format:{with: FULL_WIDTH_REGEX}
   validates :first_name, presence: true, format:{with: FULL_WIDTH_REGEX}
   validates :last_name_kana, presence: true, format:{with: KATAKANA_REGEX}
