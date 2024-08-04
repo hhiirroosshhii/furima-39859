8e17b0db10f5aeb34c3db26a80b54389dd34ef7d
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-04 13:07:29 +0900

    商品出品でデータがテーブルに格納

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,16 @@ class Item < ApplicationRecord
 
   # アクティブハッシュ
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to :category
   belongs_to :status
   belongs_to :cost
   belongs_to :area
   belongs_to :needday
 
   # バリデーション
-  validates :product, :explanation, :price, :user, presence: true
+  validates :product, :user_id, presence: true
+  validates :explanation, presence: true
   validates :category_id, :status_id, :cost_id, :area_id, :needday_id, presence: true, numericality:{other_than: 1, message: "can't be blank"}
   validates :image, presence: true
+  validates :price, presence: true, numericality:{only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
