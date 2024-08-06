091eed8552dc69f1309d21616663096932571bf7
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-06 20:13:09 +0900

    編集画面で編集することができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
+  end
+
   def destory
   end
 
