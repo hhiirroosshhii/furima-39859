44f487342b3a952a0ffbc1993cfe6aed58c32010
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-06 21:32:51 +0900

    edit画面でエラー表示
    edit.html.erbで入力不備があるとエラー表示が出るようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_item, only: [:edit, :show]
 
   def index
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    redirect_to root_path
+    redirect_to item_path
   end
 
   def destory
