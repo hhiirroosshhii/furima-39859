a0b9cb72c2653745db185f9d7352081727b73240
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-08-07 12:23:22 +0900

    editページのエラーハンドリング
    入力に不備があった場合、エラーメッセージが表示されないのを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -26,16 +26,17 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destory
   end
 
   private
-
   def item_params
     params.require(:item).permit(:product, :explanation, :category_id, :status_id, :cost_id, :area_id, :needday_id, :price,
                                  :image).merge(user_id: current_user.id)
